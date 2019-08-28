577f21c4dbc5368947d57b793629b900f2f73369
梁国强 <liangguoqiang@nationsky.com>
2019-08-28 19:38:49 +0800

    add

---
+++ weiClientSDK/weiClient.framework/Headers/weiClient.h
@@ -0,0 +1,20 @@
+//
+//  weiClient.h
+//  weiClient
+//
+//  Created by LiangGuoQiang on 2019/8/28.
+//  Copyright © 2019 lgq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for weiClient.
+FOUNDATION_EXPORT double weiClientVersionNumber;
+
+//! Project version string for weiClient.
+FOUNDATION_EXPORT const unsigned char weiClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <weiClient/PublicHeader.h>
+
+
+#import <weiClient/weiClientSDK.h>
---
+++ weiClientSDK/weiClient.framework/Headers/weiClientSDK.h
@@ -0,0 +1,17 @@
+//
+//  weiClientSDK.h
+//  weiClient
+//
+//  Created by LiangGuoQiang on 2019/8/28.
+//  Copyright © 2019 lgq. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface weiClientSDK : NSObject
++ (NSString *) currentSDKVersion;
+@end
+
+NS_ASSUME_NONNULL_END
